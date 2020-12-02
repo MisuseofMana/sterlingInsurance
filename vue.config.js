14933620eaace054df4443232aac0012f93fd2c7
Sean Yager <contact@seanyager.com>
2020-12-02 16:31:20 -0600

    trying new public path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
   module.exports = {
 	lintOnSave: false, 
     outputDir: "docs",
-    publicPath: '/docs/',
+    publicPath: '/sterlingInsurance/docs/',
     indexPath:'index.html',
     filenameHashing: false,
     chainWebpack: (config) => {
