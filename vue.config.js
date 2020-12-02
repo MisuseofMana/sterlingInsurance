c5f162768ebf615a1ba72f332408c8b58fef6d6e
Sean Yager <contact@seanyager.com>
2020-12-02 16:56:44 -0600

    didnt' work change public path and hope it works now!

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
   module.exports = {
 	lintOnSave: false, 
     outputDir: "docs",
-    publicPath: '/docs/',
+    publicPath: '/sterlingInsurance/',
     indexPath:'index.html',
     filenameHashing: false,
     chainWebpack: (config) => {
